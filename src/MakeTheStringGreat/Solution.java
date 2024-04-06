467b901e17e934294383e5576773c67a539f5004
Caio Rievers Duarte <131414483+cRievers@users.noreply.github.com>
2024-04-05 22:12:04 -0300

    Add files via upload

---
+++ src/MakeTheStringGreat/Solution.java
@@ -0,0 +1,24 @@
+package MakeTheStringGreat;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static String makeGood(String s) {
+        String str = "";
+        for(int i=0; i<=s.length()-2; i++){
+            if( ((int)s.charAt(i)) != ((int)s.charAt(i+1)+32) ){
+                str += s.charAt(i);
+            }else{
+                i++;
+            }
+        }
+        return str;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        String output = makeGood(input);
+        System.out.println(output);
+    }
+}
